681a2776a12ca321912992b854ddf360f3db0b8a
Павел Сатин <pslater.ru@gmail.com>
2021-06-07 12:42:00 +0500

    Init

---
+++ app/controller.php
@@ -0,0 +1,48 @@
+<?php
+
+//! Base controller
+class Controller {
+
+    protected
+        $db;
+
+    //! HTTP route pre-processor
+    function beforeroute($f3) {
+        $db=$this->db;
+        // Prepare user menu
+        // $f3->set('menu',
+        //     $db->exec('SELECT slug,title FROM pages ORDER BY position;'));
+        $company=new DB\SQL\Mapper($db,'contacts');
+        $company->load(array('id=?',1));
+        $f3->set('company', $company);
+
+    }
+
+    //! HTTP route post-processor
+    function afterroute() {
+        // Render HTML layout
+        echo Template::instance()->render('layout.htm');
+    }
+
+    //! Instantiate class
+    function __construct() {
+        $f3=Base::instance();
+        // Connect to the database
+        // for sqlite
+        $db=new DB\SQL($f3->get('db'));
+        // $db=new DB\SQL($f3->get('db'), $f3->get('dbuser'), $f3->get('dbpassword'));
+
+        // if (file_exists('setup.sql')) {
+        //     // Initialize database with default setup
+        //     $db->exec(explode(';',$f3->read('setup.sql')));
+        //     // Make default setup inaccessible
+        //     rename('setup.sql','setup.$ql');
+        // }
+
+        // Use database-managed sessions
+        new DB\SQL\Session($db);
+        // Save frequently used variables
+        $this->db=$db;
+    }
+
+}
---
+++ app/homepage.php
@@ -0,0 +1,88 @@
+<?php
+
+//! Front-end processor
+class HomePage extends Controller {
+
+    //! Display content page
+    function home($f3) {
+        $db=$this->db;
+
+        $f3->set('products', $db->exec('SELECT * FROM products ORDER BY id;'));
+
+        // $f3->set('pagekeywords', '');
+        $f3->set('menuactive', 'true');
+
+        $f3->set('inc','homepage.htm');
+        
+
+    }
+
+    // function about($f3) {
+    //     $db=$this->db;
+
+    //     $f3->set('contacts', $db->exec('SELECT * FROM contacts ORDER BY id;'));
+
+    //     $f3->set('menuactive', 'about');
+    //     $f3->set('inc','about.htm');
+    // }
+
+    // function contact($f3) {
+    //     $f3->set('menuactive', 'contact');
+    //     $f3->set('inc','contact.htm');
+    // }
+
+
+    //! Custom error page
+    function error($f3) {
+        $log=new Log('error.log');
+        $log->write($f3->get('ERROR.text'));
+        $trace = $f3->get('ERROR.trace');
+        if (!isset($trace)) {
+        foreach ($f3->get('ERROR.trace') as $frame)
+            if (isset($frame['file'])) {
+                // Parse each backtrace stack frame
+                $line='';
+                $addr=$f3->fixslashes($frame['file']).':'.$frame['line'];
+                if (isset($frame['class']))
+                    $line.=$frame['class'].$frame['type'];
+                if (isset($frame['function'])) {
+                    $line.=$frame['function'];
+                    if (!preg_match('/{.+}/',$frame['function'])) {
+                        $line.='(';
+                        if (isset($frame['args']) && $frame['args'])
+                            $line.=$f3->csv($frame['args']);
+                        $line.=')';
+                    }
+                }
+                // Write to custom log
+                $log->write($addr.' '.$line);
+            }
+        }
+        $f3->set('inc','error.htm');
+    }
+
+
+
+    // function notes($f3) {
+    //     $file = F3::instance()->read('notes.md');
+    //     $html = Markdown::instance()->convert($file);
+    //     $f3->set('content', $html);
+    //     $f3->set('inc','article.htm');
+
+    // }
+
+    function privacypolicy($f3) {
+        $file = F3::instance()->read('privacypolicy.md');
+        $html = Markdown::instance()->convert($file);
+        $f3->set('content', $html);
+        $f3->set('inc','policy.htm');
+
+    }
+
+    // function order($f3) {
+
+    //     $f3->set('menuactive', 'contact');
+    //     $f3->set('inc','order.htm');
+    // }
+
+}
